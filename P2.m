b895fee78827e07f2850952539520116173365d1
FelipeSerikawa <fe.sadao17@gmail.com>
2025-03-22 19:45:48 -0300

    E-Glass Epoxy

--- P2.m
+++ P2.m
@@ -1,28 +1,29 @@
 # APLICAÇÕES ESTRUTURAIS DE MATERIAIS COMPÓSITOS - ESAE001-23 PROVA 2 - TRABALHO
-clc
-# Declaração de variáveis
 
-## módulos de elasticidade
-E1 = 0;
-E2 = 0;
-## coeficiente de Poisson
-v12 = 0;
-## módulo de cisalhamento
-G12 = 0;
-## resistências da lâmina
-Xt = 0;;
-Xc = 0;;
-Yt = 0;
-Yc = 0;
-S = 0;
-## ângulo theta de orientação da lâmina em relação aos eixos x e y
-theta = deg2rad(0);
-## estado de tensão
+clc
+
+# Material: E-Glass/Epoxy
+
+# 1 - Receber as propriedades da lâmina nas direções 1 e 2
+E1 = 41; # módulos de elasticidade (GPa)
+E2 = 10.4; # módulos de elasticidade (GPa)
+v12 = 0.28; # coeficiente de Poisson
+G12 = 4.3; # módulo de cisalhamento (GPa)
+Xt = 3250; # longitudinal tensile
+Xc = 1590; # longitudinal compressive
+Yt = 62; # transverse tensile
+Yc = 200; # transverse compressive
+S = 75;  # in-plane shear
+
+# 2 - Receber o ângulo theta de orientação da lâmina em relação aos eixos x e y
+theta = deg2rad(45);
+
+# 3 - Receber o estado de tensão qualquer
 Sx = 0;
 Sy = 0;
 Txy = 0;
 
-# Calcular a partir dos dados fornecidos o estado de tensão nas direções 1 e 2
+# 4 - Calcular a partir dos dados fornecidos o estado de tensão nas direções 1 e 2
 #S1
 #S2
 #T12
