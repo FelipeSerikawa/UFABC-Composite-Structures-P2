245ea86d09aa03c8291f285d1d2f1c12e7729de2
FelipeSerikawa <fe.sadao17@gmail.com>
2025-03-22 19:16:20 -0300

    declaracao de variaveis

--- P2.m
+++ P2.m
@@ -1,1 +1,28 @@
-#APLICAÇÕES ESTRUTURAIS DE MATERIAIS COMPÓSITOS - ESAE001-23 PROVA 2 - TRABALHO
+# APLICAÇÕES ESTRUTURAIS DE MATERIAIS COMPÓSITOS - ESAE001-23 PROVA 2 - TRABALHO
+clc
+# Declaração de variáveis
+
+## módulos de elasticidade
+E1 = 0;
+E2 = 0;
+## coeficiente de Poisson
+v12 = 0;
+## módulo de cisalhamento
+G12 = 0;
+## resistências da lâmina
+Xt = 0;;
+Xc = 0;;
+Yt = 0;
+Yc = 0;
+S = 0;
+## ângulo theta de orientação da lâmina em relação aos eixos x e y
+theta = deg2rad(0);
+## estado de tensão
+Sx = 0;
+Sy = 0;
+Txy = 0;
+
+# Calcular a partir dos dados fornecidos o estado de tensão nas direções 1 e 2
+#S1
+#S2
+#T12
